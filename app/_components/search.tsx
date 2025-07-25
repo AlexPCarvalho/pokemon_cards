36d846afba077735ecc6c20be453b2bac0d0a7dd
OAKDev <oak99.dev@gmail.com>
2025-07-25 01:42:04 -0300

    feat: implement debounce on Search input

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,16 +1,39 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+
 const Search = () => {
+  const [term, setTerm] = useState('');
+  const [debouncedTerm, setDebouncedTerm] = useState('');
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedTerm(term.trim());
+    }, 800);
+
+    return () => clearTimeout(timer);
+
+    
+  }, [term]);
+
+
   return (
+    
     <div className="max-w-2xl mx-auto mb-6 px-4">
-          <div className="relative">
-            <input
-              autoFocus
-              type="text"
-              placeholder="Pesquise pelo Pokemon..."
-              className="w-full h-14 px-9 focus:border-gray rounded-full bg-white   text-lg  border-2"
-            />
-            
-          </div>
-        </div>
+      <div className="relative">
+        <input
+          
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          placeholder="Pesquise pelo Pokemon..."
+          className="w-full h-14 px-9  rounded-full bg-white text-lg border-2"
+        />
+        
+      </div>
+    </div>
   );
 };
 
